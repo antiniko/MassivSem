c9912bf44cd33c8c483e19575be01ab2e7332d35
antiniko <anti.niko@ya.ru>
2022-03-27 23:02:15 +0300

    some changes added

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -53,6 +53,7 @@ Console.Write("Сумма цифр числа "  + N + " равняется " + 
 // Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
+/*
 void Array(int N)
 {
     int[] array = new int [N];
@@ -66,3 +67,4 @@ void Array(int N)
 Console.Write("Введите размерность массива: ");
 int Size = Convert.ToInt32(Console.ReadLine());
 Array(Size);
+*/
