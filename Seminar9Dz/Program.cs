fe50db50d1b0f8b18ba16ea7b8202eaf7a3cf836
anti.niko <anti.niko@ya.ru>
2022-04-14 00:23:34 +0300

    DZ9 done

---
+++ Seminar9Dz/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1. Сделать вывод треугольника Паскаля в виде равнобедренного треугольника.
+
+Console.Write("введите порядок степени: ");
+int Counter = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < Counter; i++) // счётчик
+{
+    for (int j = Counter; j > i + 1; j--) // отступ до середины треугольника. вот он гугл всемогущийБ сократил код раза в 2
+    {
+        Console.Write(" ");
+    }
+    int triangle = 1;
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write(triangle + " ");
+        triangle = (triangle * (i - j)) / (j + 1);
+    }
+    Console.WriteLine();
+}
